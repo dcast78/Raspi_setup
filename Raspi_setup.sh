ccf58b075202b913472ca824cdef5d249eef9e0f
dcast78 <castellari@alice.it>
2013-06-01 22:24:17 +0200

    Update Raspi_setup.sh

--- Raspi_setup.sh
+++ Raspi_setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo 'alias ll="ls -lart"' > /etc/bas.bashrc
+echo 'alias ll="ls -lart"' > /etc/bash.bashrc
+
+apt-get install vim screen 
+
+sudo raspiconfig
+
+apt-get update
+apt-get upgrade
+
